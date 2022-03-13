6469c86d914ea948ca823e31774ab619db75d280
Bargurov <bargurov11@gmail.com>
2022-03-13 19:26:06 +0200

    heroku

--- API/Startup.cs
+++ API/Startup.cs
@@ -46,12 +46,33 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opt=>opt.NoReferrer());
+            app.UseXXssProtection(opt=>opt.EnabledWithBlockMode());
+            app.UseXfo(opt=>opt.Deny());
+            app.UseCspReportOnly(opt=>opt
+            .BlockAllMixedContent()
+            .StyleSources(s=>s.Self().CustomSources("https://fonts.googleapis.com"))
+            .FontSources(s=>s.Self().CustomSources("https://fonts.gstatic.com","data:"))
+            .FormActions(s=>s.Self())
+            .FrameAncestors(s=>s.Self())
+            .ImageSources(s=>s.Self().CustomSources("https://res.cloudinary.com"))
+            .ScriptSources(s=>s.Self().CustomSources("sha256-q/AfmwuMI0kcglAvjV3mdPjImxPup0GHfVf8uGkWp5U="))
+            );
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+            }else
+            {
+                app.Use(async(context,next)=>{
+                    context.Response.Headers.Add("Strict-Transport-Security","max-age=3153600");
+                    await next.Invoke();
+                });
             }
+           
 
             // app.UseHttpsRedirection();
 
